0c5d63ac6486c4d145beda88e74e7378c1128497
John Hobbs <john@velvetcache.org>
2025-06-09 14:23:20 -0500

    Initial version

---
+++ _data/events.js
@@ -0,0 +1,40 @@
+import Fetch from '@11ty/eleventy-fetch';
+import { google } from 'googleapis';
+import moment from 'moment';
+
+export default function () {
+  return Fetch(async function () {
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: process.env.GOOGLE_CALENDAR_API_KEY,
+    });
+
+    const timeMin = new Date();
+    const timeMax = new Date();
+    timeMax.setDate(timeMin.getDate() + 60);
+
+    const res = await calendar.events.list({
+      calendarId: process.env.GOOGLE_CALENDAR_ID,
+      timeMin: timeMin.toISOString(),
+      timeMax: timeMax.toISOString(), 
+      maxResults: 100,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+
+    return res.data.items.map((event) => {
+      const start = moment(event.start.dateTime || event.start.date);
+      const end = moment(event.end.dateTime || event.end.date);
+
+      return {
+        date: start.format('dddd, MMMM Do'),
+        time: `${start.format('h:mm')} - ${end.format('h:mm a')}`,
+        title: event.summary,
+        description: event.description,
+      }
+    });
+  }, {
+    requestId: 'calendar',
+    duration: '12h',
+  });
+}
---
+++ eleventy.config.js
@@ -0,0 +1,45 @@
+import * as sass from 'sass';
+import path from 'node:path';
+import browserslist from 'browserslist'
+import { transform, browserslistToTargets } from 'lightningcss'
+
+export default function(eleventyConfig) {
+  eleventyConfig.addTemplateFormats("scss");
+
+  // From https://11ty.rocks/posts/process-css-with-lightningcss/
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      // Skip files like _fileName.scss
+      let parsed = path.parse(inputPath);
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      // Run file content through Sass
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || "."],
+        sourceMap: false, // or true, your choice!
+      });
+
+      // Allow included files from @use or @import to
+      // trigger rebuilds when using --incremental
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      let targets = browserslistToTargets(browserslist("> 0.2% and not dead"));
+
+      return async () => {
+        return transform({
+          code: Buffer.from(result.css),
+          minify: true,
+          sourceMap: false,
+          targets,
+        }).code;
+      };
+    },
+  });
+
+  eleventyConfig.addFilter("splitParagraphs", function(value) {
+    return value.split("\n").filter((p) => p.trim() !== '');
+  });
+};
