af782272f63009f9dbf827f1c701669e62a35970
John Hobbs <john@velvetcache.org>
2025-06-10 09:51:00 -0500

    Make filter script external

---
+++ static/script.js
@@ -0,0 +1,42 @@
+(function () { 
+  function filterEvents(search) {
+    const events = document.querySelectorAll('.event');
+    events.forEach(event => {
+      if (event.innerText.toLowerCase().includes(search)) {
+        event.classList.remove('hidden');
+      } else {
+        event.classList.add('hidden');
+      }
+    });
+  }
+
+  function hideEmptyDays() {
+    const days = document.querySelectorAll('.day');
+    days.forEach(day => {
+      if (day.querySelector('.event:not(.hidden)')) {
+        day.classList.remove('hidden');
+      } else {
+        day.classList.add('hidden');
+      }
+    });
+  }
+
+  function showNoResultsWarning() {
+    const noEvents = document.getElementById('no-events');
+    if(document.querySelector('.day:not(.hidden)')) {
+      noEvents.classList.add('hidden');
+    } else {
+      noEvents.classList.remove('hidden');
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded',function () {
+    const input = document.querySelector('input[type="search"]');
+    input.addEventListener('input', function(event) {
+      const filter = event.target.value.toLowerCase();
+      filterEvents(filter);
+      hideEmptyDays();
+      showNoResultsWarning();
+    });
+  });
+})();
