8852287a2a11e77dbd53b779d08fd5b36b1b4e1c
John Hobbs <john@velvetcache.org>
2025-06-10 09:50:31 -0500

    Localize build time to central US

--- _data/lastUpdated.js
+++ _data/lastUpdated.js
@@ -1,3 +1,3 @@
-import moment from 'moment';
+import moment from 'moment-timezone';
 
-export default moment().format('MMMM Do YYYY, h:mm:ss a');
+export default moment().tz('America/Chicago').format('MMMM Do YYYY, h:mm a');
